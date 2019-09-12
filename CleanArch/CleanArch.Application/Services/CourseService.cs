66b1e0681f23b6ab1cb1c925087714f1e76b8bd4
dekams <kamayeric@gmail.com>
2019-09-12 08:01:25 +0200

    Commit Implementation of Service and Repository GetCourses

---
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly ICourseRepository courseRepository;
+
+        public CourseService(ICourseRepository courseRepository) => this.courseRepository = courseRepository;
+
+        public async Task<CourseViewModel> GetCourses()
+        {
+            var vm = new CourseViewModel
+            {
+                Courses = await courseRepository.GetCourses()
+            };
+
+            return vm;
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,21 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDBContext context;
+
+        public CourseRepository(UniversityDBContext context) => this.context = context;
+
+        public async Task<IEnumerable<Course>> GetCourses()
+        {
+            return await context.Courses.ToListAsync();
+        }
+    }
+}
